611a328491dbb5aa7701dcf049add400955e15b4
Fernando Da Rós <fernando.ros@stone.com.br>
2021-02-21 15:27:39 -0300

    chore: improve log

--- cmd/main.go
+++ cmd/main.go
@@ -8,18 +8,22 @@ import (
 
 func main() {
 	log := logrus.New()
+
 	s1 := investments.Scenario{
-		AnnualYield: 10,
-		// InitialBalance: 10000.00,
-		MonthApply: 1000.00,
-		Years:      1,
+		AnnualYield:    6,
+		InitialBalance: 10000.00,
+		// MonthApply:     1000.00,
+		Years: 20,
 	}
-	log.Infoln(s1.String())
-	log.Infoln(s1.GetMonthYield())
+	// log.Infoln(s1.String())
+	// log.Infoln(s1.GetMonthYield())
 
 	records := s1.Run()
 	for i, r := range records {
-		log.WithFields(logrus.Fields{"Month": i + 1, "Balance": r.String()}).Infoln("aa")
+		log.WithFields(logrus.Fields{
+			"balance":               r,
+			"month income (.6 a.a)": (r * .06) / 12,
+		}).Infoln("Investment simulation for month", i+1)
 	}
 
 	// TODO generate chart https://github.com/gonum/plot
