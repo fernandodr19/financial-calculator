715cc761737e6f80a96ec0e6aa307a1356112057
Fernando Da Rós <fernando.ros@stone.com.br>
2021-02-21 06:27:09 -0300

    feat: calculate month yield

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"math"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	log := logrus.New()
-	anualYield := 10
-	log.Infoln(anualYield)
+	annualYield := 10.0
+	log.Infoln(annualYield)
+	log.Infoln(calculateMonthYield(annualYield))
+}
+
+func calculateMonthYield(annualYield float64) float64 {
+	return math.Pow(annualYield, 1.0/12)
 }
