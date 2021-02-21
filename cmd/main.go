f725d31fb3d649cc24ce781b0b4988059f2f0cfe
Fernando Da Rós <fernando.ros@stone.com.br>
2021-02-21 07:13:30 -0300

    feat: run scenario

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"main/pkg/investments"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	log := logrus.New()
-	annualYield := Profit{}
-	log.Infoln(annualYield)
-	log.Infoln(calculateMonthYield(annualYield))
+	s1 := investments.Scenario{
+		AnnualYield:    10,
+		InitialBalance: 10000.00,
+		// MonthApply:     1000.00,
+		Years: 1,
+	}
+	log.Infoln(s1.String())
+	log.Infoln(s1.GetMonthYield())
+
+	records := s1.Run()
+	for i, r := range records {
+		log.WithFields(logrus.Fields{"Month": i + 1, "Balance": fmt.Sprintf("%.2f", r)}).Infoln("aa")
+	}
 }
---
+++ pkg/investments/investments.go
@@ -0,0 +1,35 @@
+package investments
+
+import (
+	"fmt"
+	"math"
+)
+
+type Scenario struct {
+	AnnualYield    float64
+	InitialBalance float64
+	MonthApply     float64
+	Years          int
+}
+
+func (s Scenario) Run() []float64 {
+	months := s.Years * 12
+
+	records := make([]float64, months)
+
+	currentBalance := s.InitialBalance
+	for i := 0; i < months; i++ {
+		currentBalance = (currentBalance * (1 + s.GetMonthYield()/100)) + s.MonthApply
+		records[i] = currentBalance
+	}
+
+	return records
+}
+
+func (s Scenario) String() string {
+	return fmt.Sprintf("%.2f%% a.a (%.2f%% a.m)", s.AnnualYield, s.GetMonthYield())
+}
+
+func (s Scenario) GetMonthYield() float64 {
+	return math.Pow(s.AnnualYield, 1.0/12)
+}
