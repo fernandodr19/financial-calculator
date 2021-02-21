dcdc5a53fc012446c04f86d086877b58ce0cbf54
Fernando Da Rós <fernando.ros@stone.com.br>
2021-02-21 06:42:13 -0300

    refact: organizer dirs

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"math"
-
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	log := logrus.New()
-	annualYield := 10.0
+	annualYield := Profit{}
 	log.Infoln(annualYield)
 	log.Infoln(calculateMonthYield(annualYield))
-}
-
-func calculateMonthYield(annualYield float64) float64 {
-	return math.Pow(annualYield, 1.0/12)
 }
